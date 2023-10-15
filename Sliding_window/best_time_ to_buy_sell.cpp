19e1eee41ee6be1f18da46d03277de8d2c468cac
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-10-15 14:44:51 +0530

    Update best_time_ to_buy_sell.cpp

--- Sliding_window/best_time_ to_buy_sell.cpp
+++ Sliding_window/best_time_ to_buy_sell.cpp
@@ -1,28 +1,64 @@
 /*
-	Complexity: Easy
     Given array prices, return max profit w/ 1 buy & 1 sell
     Ex. prices = [7,1,5,3,6,4] -> 5 (buy at $1, sell at $6)
-
     For each, get diff b/w that & min value before, store max
-
-    Time: O(n)
-    Space: O(1)
 */
+Brute Force Approach:
+Intuition: We can simply use 2 loops and track every transaction and maintain a variable maxPro to contain the max value among all transactions.
+Approach: 
+Use a for loop of ‘i’ from 0 to n.
+Use another for loop of j from ‘i+1’ to n.
+If arr[j] > arr[i] , take the difference and compare  and store it in the maxPro variable.
+Return maxPro.
+Code:
+#include<bits/stdc++.h>
+using namespace std;
 
-class Solution 
-{
-public:
-    int maxProfitGain(vector<int>& prices) 
-	{
-        int minBuy = prices[0]; //buy should be minimum
-        int maxProfit = 0; //profit should be max
-        
-        for (int i = 1; i < prices.size(); i++) 
-		{
-            minBuy = min(minBuy, prices[i]);
-            maxProfit = max(maxProfit, prices[i] - minBuy);
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] > arr[i]) {
+            maxPro = max(arr[j] - arr[i], maxPro);
+            }
         }
-        
-        return maxProfit;
+        }
+
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
+Time complexity: O(n^2)
+Space Complexity: O(1)
+--------------------------------------------------------------------------------------------------------
+Sliding window:
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxProfit(vector<int> &arr) {
+    int maxPro = 0;
+    int n = arr.size();
+    int minPrice = INT_MAX;
+
+    for (int i = 0; i < arr.size(); i++) {
+        minPrice = min(minPrice, arr[i]);
+        maxPro = max(maxPro, arr[i] - minPrice);
     }
-};
+    
+    return maxPro;
+}
+
+int main() {
+    vector<int> arr = {7,1,5,3,6,4};
+    int maxPro = maxProfit(arr);
+    cout << "Max profit is: " << maxPro << endl;
+}
+Time complexity: O(n)
+Space Complexity: O(1)
