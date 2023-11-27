a770430a94abcdec019be3da5575227153070fe3
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-11-27 17:11:56 +0530

    Create 4_sum.cpp

---
+++ Array/1D-Array/Medium/4_sum.cpp
@@ -0,0 +1,79 @@
+Problem Statement: Given an array of N integers, your task is to find unique quads that add up to give a 
+target value. In short, you need to return an array of all the unique quadruplets [arr[a], arr[b], arr[c],
+arr[d]] such that their sum is equal to a given target.
+Example 1:
+Input Format: arr[] = [1,0,-1,0,-2,2], target = 0
+Result: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+Explanation: We have to find unique quadruplets from the array such that the sum of those elements is equal to the target sum given that is 0. The result obtained is such that the sum of the quadruplets yields 0.
+
+Example 2:
+Input Format: arr[] = [4,3,3,4,4,2,1,2,1,1], target = 9
+Result: [[1,1,3,4],[1,2,2,4],[1,2,3,3]]
+Explanation: The sum of all the quadruplets is equal to the target i.e. 9.
+------------------------------------------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> fourSum(vector<int>& nums, int target) {
+    int n = nums.size(); //size of the array
+    vector<vector<int>> ans;
+
+    // sort the given array:
+    sort(nums.begin(), nums.end());
+
+    //calculating the quadruplets:
+    for (int i = 0; i < n; i++) {
+        // avoid the duplicates while moving i:
+        if (i > 0 && nums[i] == nums[i - 1]) continue;
+        for (int j = i + 1; j < n; j++) {
+            // avoid the duplicates while moving j:
+            if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+            // 2 pointers:
+            int k = j + 1;
+            int l = n - 1;
+            while (k < l) {
+                long long sum = nums[i];
+                sum += nums[j];
+                sum += nums[k];
+                sum += nums[l];
+                if (sum == target) {
+                    vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                    ans.push_back(temp);
+                    k++; l--;
+
+                    //skip the duplicates:
+                    while (k < l && nums[k] == nums[k - 1]) k++;
+                    while (k < l && nums[l] == nums[l + 1]) l--;
+                }
+                else if (sum < target) k++;
+                else l--;
+            }
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
+    vector<int> nums = {4, 3, 3, 4, 4, 2, 1, 2, 1, 1};
+    int target = 9;
+    vector<vector<int>> ans = fourSum(nums, target);
+    cout << "The quadruplets are: \n";
+    for (auto it : ans) {
+        cout << "[";
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "] ";
+    }
+    cout << "\n";
+    return 0;
+}
+
+Complexity Analysis
+Time Complexity: O(N3), where N = size of the array.
+Reason: Each of the pointers i and j, is running for approximately N times. And both the pointers k and l combined can run for approximately N times including the operation of skipping duplicates. So the total time complexity will be O(N3). 
+
+Space Complexity: O(no. of quadruplets), This space is only used to store the answer. We are not using any extra space to solve this problem. So, from that perspective, space complexity can be written as O(1).
