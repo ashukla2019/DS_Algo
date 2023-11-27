f40183554909de52ca68bfcb2cd687493250864f
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-11-27 18:02:57 +0530

    Create count_reverse_pairs.cpp

---
+++ Array/1D-Array/Hard/count_reverse_pairs.cpp
@@ -0,0 +1,144 @@
+Problem Statement: Given an array of numbers, you need to return the count of reverse pairs. 
+Reverse Pairs are those pairs where i<j and arr[i]>2*arr[j].
+
+Example 1:
+
+Input: N = 5, array[] = {1,3,2,3,1)
+
+Output: 2 
+
+Explanation: The pairs are (3, 1) and (3, 1) as from both the pairs the condition arr[i] > 2*arr[j] is satisfied.
+
+Example 2:
+
+Input: N = 4, array[] = {3,2,1,4}
+
+Output: 1
+
+Explaination: There is only 1 pair  ( 3 , 1 ) that satisfy the condition arr[i] > 2*arr[j]  
+---------------------------------------------------------------------------------------------------------------------
+Brute Force Approach:
+The steps are as follows:
+
+First, we will run a loop(say i) from 0 to N-1 to select the a[i].
+As index j should be greater than index i, inside loop i, we will run another loop i.e. j from i+1 to N-1, and select the element a[j].
+Inside this second loop, we will check if a[i] > 2*a[j] i.e. if a[i] and a[j] can be a pair. If they satisfy the condition, we will increase the count by 1.
+Finally, we will return the count i.e. the number of such pairs.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPairs(vector<int>&a, int n) {
+
+    // Count the number of pairs:
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (a[i] > 2 * a[j]) cnt++;
+        }
+    }
+    return cnt;
+}
+
+int team(vector <int> & skill, int n) {
+    return countPairs(skill, n);
+}
+
+int main()
+{
+    vector<int> a = {4, 1, 2, 3, 1};
+    int n = 5;
+    int cnt = team(a, n);
+    cout << "The number of reverse pair is: "
+         << cnt << endl;
+    return 0;
+}
+Time Complexity: O(N2), where N = size of the given array.
+Reason: We are using nested loops here and those two loops roughly run for N times.
+Space Complexity: O(1) as we are not using any extra space to solve this problem.  
+---------------------------------------------------------------------------------------------------------------------  
+
+Optimal Approach: 
+Intuition: 
+In order to solve this problem we will use the merge sort algorithm like we used in the problem count inversion with a slight modification of the merge() function. But in this case, the same logic will not work. In order to understand this, we need to deep dive into the merge() function.
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void merge(vector<int> &arr, int low, int mid, int high) {
+    vector<int> temp; // temporary array
+    int left = low;      // starting index of left half of arr
+    int right = mid + 1;   // starting index of right half of arr
+
+    //storing elements in the temporary array in a sorted manner//
+
+    while (left <= mid && right <= high) {
+        if (arr[left] <= arr[right]) {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else {
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+
+    // if elements on the left half are still left //
+
+    while (left <= mid) {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    //  if elements on the right half are still left //
+    while (right <= high) {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    // transfering all elements from temporary to arr //
+    for (int i = low; i <= high; i++) {
+        arr[i] = temp[i - low];
+    }
+}
+
+int countPairs(vector<int> &arr, int low, int mid, int high) {
+    int right = mid + 1;
+    int cnt = 0;
+    for (int i = low; i <= mid; i++) {
+        while (right <= high && arr[i] > 2 * arr[right]) right++;
+        cnt += (right - (mid + 1));
+    }
+    return cnt;
+}
+
+int mergeSort(vector<int> &arr, int low, int high) {
+    int cnt = 0;
+    if (low >= high) return cnt;
+    int mid = (low + high) / 2 ;
+    cnt += mergeSort(arr, low, mid);  // left half
+    cnt += mergeSort(arr, mid + 1, high); // right half
+    cnt += countPairs(arr, low, mid, high); //Modification
+    merge(arr, low, mid, high);  // merging sorted halves
+    return cnt;
+}
+
+int team(vector <int> & skill, int n)
+{
+    return mergeSort(skill, 0, n - 1);
+}
+
+int main()
+{
+    vector<int> a = {4, 1, 2, 3, 1};
+    int n = 5;
+    int cnt = team(a, n);
+    cout << "The number of reverse pair is: "
+         << cnt << endl;
+    return 0;
+}
+Time Complexity: O(2N*logN), where N = size of the given array.
+Reason: Inside the mergeSort() we call merge() and countPairs() except mergeSort() itself. Now, inside the function countPairs(), though we are running a nested loop, we are actually iterating the left half once and the right half once in total. That is why, the time complexity is O(N). And the merge() function also takes O(N). The mergeSort() takes O(logN) time complexity. Therefore, the overall time complexity will be O(logN * (N+N)) = O(2N*logN).
+Space Complexity: O(N), as in the merge sort We use a temporary array to store elements in sorted order.  
