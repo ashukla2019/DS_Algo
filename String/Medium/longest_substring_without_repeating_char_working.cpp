1bf1e3d890aeb4a6bea1433511ad733d311fbd70
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-13 15:16:45 +0530

    Update longest_substring_without_repeating_char_working.cpp

--- String/Medium/longest_substring_without_repeating_char_working.cpp
+++ String/Medium/longest_substring_without_repeating_char_working.cpp
@@ -1,4 +1,8 @@
-int lengthOfLongestSubstring(string s) 
+ #include <iostream>
+ #include <string>
+ #include <unordered_map>
+ using namespace std;
+ int lengthOfLongestSubstring(string s) 
 {
         int n=s.length();
         int left=0;
@@ -7,8 +11,23 @@ int lengthOfLongestSubstring(string s)
         int maxcnt=0;
         for(right=0;right<n;right++){
         // if char s[right] is already present in map and index of s[right] >= left then update the left pointer to previoues accour of that s[right] char + 1 
-            if(m.find(s[right])!=m.end() && m[s[right]]>=left){
+            if(m.find(s[right])!=m.end() && m[s[right]]>=left)
+            {
                 left=m[s[right]]+1;
+                cout<<"m[s[right]]"<<m[s[right]]<<endl;
+                cout<<"left==========="<<left<<endl;
+                /*
+                m[s[right]]0
+                left===========1
+                m[s[right]]1
+                left===========2
+                m[s[right]]2
+                left===========3
+                m[s[right]]4
+                left===========5
+                m[s[right]]6
+                left===========7
+                */
             }
             // everytime update the index for s[right]
             m[s[right]]=right;
@@ -17,5 +36,11 @@ int lengthOfLongestSubstring(string s)
         } 
         return maxcnt;
     }
-Input: s = "abcabcbb"
-Output: 3
+    
+int main()
+{
+    int len = lengthOfLongestSubstring("abcabcbb");
+    cout<<"result"<<len<<std::endl;
+    return 0;
+}
+O/p: 3
