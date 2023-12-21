88325f128eef8eda0ba8e1af7dbf14c4f66d8cfa
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-21 11:03:57 +0530

    Update stack_implementation_using_queue.cpp

--- Stack&Queue/Easy/stack_implementation_using_queue.cpp
+++ Stack&Queue/Easy/stack_implementation_using_queue.cpp
@@ -9,50 +9,47 @@ push(x)
 Pop():
 remove Q1.top()
 */
-
-#include <bits/stdc++.h>
-using namespace std;
-
-class Stack {
-	// Two inbuilt queues
-	queue<int> q1, q2;
-
+#include<queue>
+class MyStack 
+{
+queue<int>q1,q2;    
 public:
-	void push(int x)
-	{
-		// Push x first in empty q2
-		q2.push(x);
-
-		// Push all the remaining
-		// elements in q1 to q2.
-		while (!q1.empty()) {
-			q2.push(q1.front());
-			q1.pop();
-		}
-
-		// swap the names of two queues
-		queue<int> q = q1;
-		q1 = q2;
-		q2 = q;
-	}
-
-	void pop()
-	{
-		// if no elements are there in q1
-		if (q1.empty())
-			return;
-		q1.pop();
-	}
-
-	int top()
-	{
-		if (q1.empty())
-			return -1;
-		return q1.front();
-	}
-
-	int size() { return q1.size(); }
+    MyStack() 
+    {
+        
+    }
+    
+    void push(int x) 
+    {
+        q2.push(x);
+        while(!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+        queue<int>q;
+        q=q1;
+        q1=q2;
+        q2=q;
+    }
+    int pop() 
+    {
+       int x=q1.front();
+       q1.pop();
+       return x;
+    }
+    
+    int top() 
+    {
+        return q1.front();
+    }
+    
+    bool empty() 
+    {
+        return q1.size()==0;
+    }
 };
+
 
 // Driver code
 int main()
