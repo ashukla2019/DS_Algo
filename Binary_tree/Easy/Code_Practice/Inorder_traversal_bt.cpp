63d941411d702221346045bfb97d452b3ec69c4d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-28 18:33:34 +0530

    Create Inorder_traversal_bt.cpp

---
+++ Binary_tree/Easy/Code_Practice/Inorder_traversal_bt.cpp
@@ -0,0 +1,40 @@
+Recursive Approach:
+Time complexity: O(n)
+Space complexity: O(n)
+
+
+
+  -------------------------------------------------------------------------
+//Recursive Approach:
+Time complexity: O(n)
+Space complexity: O(n)
+vector < int > inorderTraversal(TreeNode * curr) 
+  {
+    vector<int>ans; 
+    //Base case: 
+    if(curr==NULL)
+        return ans;
+    stack<TreeNode*>s;
+    //This is required to make recursive environemnt
+    while(true)
+    {
+        //We iterate through leftsubtree and push node to stack      until we get null
+        //If curr is null, then print/push val to vector and pop it
+        //Now iterate through right sub tree until we get null
+        if(curr != NULL)
+        {
+            s.push(curr);
+            curr = curr->left;
+        }
+        else
+        {
+            if(s.empty())
+                break;
+            curr = s.top();
+            ans.push_back(curr->val);
+            s.pop();
+            curr = curr->right;    
+        }
+    }
+    return ans;
+  }
