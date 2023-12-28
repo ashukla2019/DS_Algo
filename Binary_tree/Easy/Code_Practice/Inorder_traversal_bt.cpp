524eebf84c79d6e606e130c4144950e4e82a46dd
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-28 18:35:35 +0530

    Update Inorder_traversal_bt.cpp

--- Binary_tree/Easy/Code_Practice/Inorder_traversal_bt.cpp
+++ Binary_tree/Easy/Code_Practice/Inorder_traversal_bt.cpp
@@ -2,7 +2,14 @@ Recursive Approach:
 Time complexity: O(n)
 Space complexity: O(n)
 
+void inOrderTrav(node * curr, vector < int > & inOrder) {
+  if (curr == NULL)
+    return;
 
+  inOrderTrav(curr -> left, inOrder);
+  inOrder.push_back(curr -> data);
+  inOrderTrav(curr -> right, inOrder);
+}
 
   -------------------------------------------------------------------------
 //Recursive Approach:
