988054ee5bd9a64d369de83c9befcad5f131b8b9
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-28 19:08:52 +0530

    Create preorder_traversal_bt.cpp

---
+++ Binary_tree/Easy/Code_Practice/preorder_traversal_bt.cpp
@@ -0,0 +1,31 @@
+ vector<int> preorderTraversal(TreeNode* root) 
+    {
+        vector<int>ans;
+        // Base Case
+    if (root == NULL)
+        return ans;
+ 
+    // Create an empty stack and push root to it
+    stack<TreeNode*> nodeStack;
+    nodeStack.push(root);
+ 
+    /* Pop all items one by one. Do following for every popped item
+       a) print it
+       b) push its right child
+       c) push its left child
+    Note that right child is pushed first so that left is processed first */
+    while (!nodeStack.empty()) 
+    {
+        // Pop the top item from stack and print it
+        TreeNode* node = nodeStack.top();
+        ans.push_back(node->val);
+        nodeStack.pop();
+ 
+        // Push right and left children of the popped node to stack
+        if (node->right)
+            nodeStack.push(node->right);
+        if (node->left)
+            nodeStack.push(node->left);
+    }
+    return ans;
+  }
