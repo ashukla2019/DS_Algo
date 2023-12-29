d0f2ec2d5237dd2c60d388de90621bf6e2463ae6
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-29 17:56:21 +0530

    Create top_view_bt.cpp

---
+++ Binary_tree/Medium/Code_Practice/top_view_bt.cpp
@@ -0,0 +1,35 @@
+vector<int> topView(Node *root)
+    {
+         vector<int> ans; 
+        if(root == NULL) return ans; 
+        map<int,int> mpp; 
+        queue<pair<Node*, int>> q; 
+        q.push({root, 0}); 
+        while(!q.empty()) {
+            auto it = q.front(); 
+            q.pop(); 
+            Node* node = it.first; 
+            int line = it.second; 
+			
+			//Check if level/line is not already entered then only add it in map
+			//otherwise top view data will be overwritten by bottom view nodes.
+			if(mpp.find(line) == mpp.end())
+			{
+				mpp[line] = node->data; 
+			}
+            
+            if(node->left != NULL) {
+                q.push({node->left, line-1}); 
+            }
+            if(node->right != NULL) {
+                q.push({node->right, line + 1}); 
+            }
+            
+        }
+        
+        for(auto it : mpp) {
+            ans.push_back(it.second); 
+        }
+        return ans;  
+    
+    }
