d9198db32cac51f94900e1917bbdf6944a083f89
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-29 17:24:06 +0530

    Update right_view_bt.cpp

--- Binary_tree/Easy/Code_Practice/right_view_bt.cpp
+++ Binary_tree/Easy/Code_Practice/right_view_bt.cpp
@@ -6,9 +6,8 @@ void checkRightView(Node* root, int level, vector<int>&ans)
 		ans.push_back(root->data);
 	
     checkRightView(root->right, level+1, ans);	
-	checkRightView(root->left, level+1, ans);	
+    checkRightView(root->left, level+1, ans);	
 	
-		
 }
 
 vector<int> rightView(Node *root)
