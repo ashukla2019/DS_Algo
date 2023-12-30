0ae4bd095c9979ac1c8b8893d945db5a4a339dc1
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2023-12-30 19:55:50 +0530

    Update Rabincarp_algo.cpp

--- String/Medium/Rabincarp_algo.cpp
+++ String/Medium/Rabincarp_algo.cpp
@@ -1,57 +1,44 @@
-  private int prime = 101;
-    
-    public int patternSearch(char[] text, char[] pattern){
-        int m = pattern.length;
-        int n = text.length;
-        long patternHash = createHash(pattern, m - 1);
-        long textHash = createHash(text, m - 1);
-        for (int i = 1; i <= n - m + 1; i++) {
-            if(patternHash == textHash && checkEqual(text, i - 1, i + m - 2, pattern, 0, m - 1)) {
-                return i - 1;
-            }
-            if(i < n - m + 1) {
-                textHash = recalculateHash(text, i - 1, i + m - 1, textHash, m);
-            }
-        }
-        return -1;
-    }
-    
-    private long recalculateHash(char[] str,int oldIndex, int newIndex,long oldHash, int patternLen) {
-        long newHash = oldHash - str[oldIndex];
-        newHash = newHash/prime;
-        newHash += str[newIndex]*Math.pow(prime, patternLen - 1);
-        return newHash;
-    }
-    
-    private long createHash(char[] str, int end){
-        long hash = 0;
-        for (int i = 0 ; i <= end; i++) {
-            hash += str[i]*Math.pow(prime,i);
-        }
-        return hash;
-    }
-    
-    bool checkEqual(char str1[],int start1,int end1, char str2[],int start2,int end2){
-        if(end1 - start1 != end2 - start2) {
-            return false;
-        }
-        while(start1 <= end1 && start2 <= end2){
-            if(str1[start1] != str2[start2]){
-                return false;
-            }
-            start1++;
-            start2++;
-        }
-        return true;
-    }
+#include <bits/stdc++.h>
+#define ll long long int
+#define prime 119
+using namespace std;
 
-int main()
-{
-        RabinKarpSearch rks = new RabinKarpSearch();
-        System.out.println(rks.patternSearch("TusharRoy".toCharArray(), "sharRoy".toCharArray()));
-        System.out.println(rks.patternSearch("TusharRoy".toCharArray(), "Roy".toCharArray()));
-        System.out.println(rks.patternSearch("TusharRoy".toCharArray(), "shas".toCharArray()));
-        System.out.println(rks.patternSearch("TusharRoy".toCharArray(), "usha".toCharArray()));
-        System.out.println(rks.patternSearch("TusharRoy".toCharArray(), "Tus".toCharArray()));
+ll createhash(string str, int n) {
+    ll result = 0;
+    for (int i = 0; i < n; i++) {
+        result += (ll)(str[i] * (ll)pow(prime, i));
     }
+    return result;
+}
+ll recalculate_hash(string str, int oldindex, int newindex, ll oldhash, int pathlength) {
+    ll newhash = oldhash - str[oldindex];
+    newhash = newhash / prime;
+    newhash += (ll)(str[newindex] * (ll)(pow(prime, pathlength - 1)));
+    return newhash;
+}
+bool checkequal(string str1, string str2, int start1, int end1, int start2, int end2) {
+    if (end1 - start1 != end2 - start2) return false;
+    while (start1 <= end1 and start2 <= end2) {
+        if (str1[start1] != str2[start2]) return false;
+        start1++;
+        start2++;
+    }
+    return true;
+}
+int main() {
+    string str = "ababcabdab";
+    string pat = "abd";
+    // this will give the initial hash value of the string and the pattern
+    ll pathash = createhash(pat, pat.length());
+    ll strhash = createhash(str, pat.length());
+    for (int i = 0; i <= str.length() - pat.length() - 1; i++) {
+        if (pathash == strhash and
+            checkequal(str, pat, i, i + pat.length() - 1, 0, pat.length() - 1)) {
+            cout << i << endl;
+        }
+        if (i < str.length() - pat.length()) {
+            strhash = recalculate_hash(str, i, i + pat.length(), strhash, pat.length());
+        }
+    }
+    return 0;
 }
