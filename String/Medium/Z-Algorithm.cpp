3b781eedadaf2e60aaa03844a229bf19b4436408
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-01-03 18:34:33 +0530

    Update Z-Algorithm.cpp

--- String/Medium/Z-Algorithm.cpp
+++ String/Medium/Z-Algorithm.cpp
@@ -1,3 +1,11 @@
+/*
+Example:
+Index            0   1   2   3   4   5   6   7   8   9  10  11 
+Text             a   a   b   c   a   a   b   x   a   a   a   z
+Z values         X   1   0   0   3   1   0   0   2   2   1   0 
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 
