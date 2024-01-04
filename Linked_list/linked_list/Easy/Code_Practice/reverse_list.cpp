65967b302456b5524d69e98a6f09299b3f141ed7
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-01-04 15:37:25 +0530

    Create reverse_list.cpp

---
+++ Linked_list/linked_list/Easy/Code_Practice/reverse_list.cpp
@@ -0,0 +1,31 @@
+/*
+We will take 3 pointers:
+  1)current will point to head
+  2)temp to hold the current->next, so that we can move ahead and current can follow to it
+  3)prev: it will be used to store current->next
+ Logic: 
+ we have to traverse current until it becomes null, and follow below steps:
+ 1) Put temp into current->next(temp=current->next)
+ 2) Now reverse current->next link by putting prev to current->next
+ 3) Now move prev to current and current to temp
+ 4) Once current is null, put header to prev node.
+
+Time complexity:O(n)
+Space complexity: O(1)
+*/
+  
+ListNode* reverseList(ListNode* head) 
+    {
+        ListNode* current=head;
+        ListNode* prev=NULL;
+        ListNode* temp;
+        while(current !=NULL)
+        {
+            temp = current->next;
+            current->next=prev;
+            prev=current;
+            current=temp;
+        }
+        head=prev;
+        return head;
+    }
