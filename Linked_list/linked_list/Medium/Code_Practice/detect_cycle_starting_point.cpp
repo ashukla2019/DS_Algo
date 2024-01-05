5b4ab477ab0de8e18275449796cfa2b35580742d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-01-05 17:51:07 +0530

    Create detect_cycle_starting_point.cpp

---
+++ Linked_list/linked_list/Medium/Code_Practice/detect_cycle_starting_point.cpp
@@ -0,0 +1,21 @@
+ListNode *detectCycle(ListNode *head) 
+    {
+        ListNode* fast=head;
+        ListNode* slow=head;
+        while(fast && fast->next)
+        {
+            fast=fast->next->next;
+            slow=slow->next;
+            if(slow==fast)
+            {
+                slow=head;
+                while(slow !=fast)
+                {
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+                
+            }
+        }
+        return slow;
+    }
