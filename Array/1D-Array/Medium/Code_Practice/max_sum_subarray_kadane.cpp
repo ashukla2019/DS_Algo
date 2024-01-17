f90a45cec7e1067448e1e0de8407ef2484eeae31
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-01-17 16:22:39 +0530

    Update max_sum_subarray_kadane.cpp

--- Array/1D-Array/Medium/Code_Practice/max_sum_subarray_kadane.cpp
+++ Array/1D-Array/Medium/Code_Practice/max_sum_subarray_kadane.cpp
@@ -1,1 +1,28 @@
+/*
+1) Take two variables: maxsum(will have final sum) and currentsum(sum till current iteration)
+2) Iterate through all array values and add them: currentsum=currentsum+nums[i]
+3) In every iteration check if currentsum>maxsum then update maxsum with this value
+*/  
 
+int maxSubArray(vector<int>& nums) 
+    {
+        int maxSum = 0;
+        int currentSum = 0;
+        
+        for (int i = 0; i < nums.size(); i++) 
+        {
+            currentSum += nums[i];
+            
+            if (currentSum > maxSum) 
+            {
+                maxSum = currentSum;
+            }
+            
+            if (currentSum < 0) 
+            {
+                currentSum = 0;
+            }
+        }
+        
+        return maxSum;
+    }
