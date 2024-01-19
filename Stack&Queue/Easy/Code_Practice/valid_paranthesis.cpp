4495e8ef1f195236ae013d582c3c28b3e2340baf
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-01-19 15:09:45 +0530

    Update valid_paranthesis.cpp

--- Stack&Queue/Easy/Code_Practice/valid_paranthesis.cpp
+++ Stack&Queue/Easy/Code_Practice/valid_paranthesis.cpp
@@ -1,18 +1,24 @@
-bool isValid(string s) {
+ bool isValid(string s) {
         stack<char> st;
-        for(auto i:s){
-            if(i == '(' || i == '{' || i == '[' ){  // If new bracket is introduced then me push it into our stack
+        for(auto i:s)
+        {
+            if(i=='(' || i=='{'|| i=='[' )
+            {
                 st.push(i);
             }
-            else if(st.empty()){  // If our stack is empty and we get closing brackets i.e. ( { [ 
-                return false;
+            else
+            {
+                if(st.empty() || (st.top()=='(' && i!=')') ||(st.top()=='{' && i!='}')||(st.top()=='[' && i!=']'))
+                {
+                    return false;
+                }
+                else
+                {
+                    st.pop();
+                }
             }
-            else if(st.top() == '(' && i == ')' || st.top() == '{' && i == '}' || st.top() == '[' && i == ']'){  // If our stack is not empty then we check if the top of stack is the opening of the same bracket
-                st.pop();
-            }
-            else{
-                return false;
-            }
-        }        
+            
+        }
         return st.empty();
+        
     }
