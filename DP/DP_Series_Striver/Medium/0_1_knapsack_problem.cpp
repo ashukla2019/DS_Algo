2717f5079e59620fa66096d84ffb22d1cb133002
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-02 12:07:07 +0530

    Update 0_1_knapsack_problem.cpp

--- DP/DP_Series_Striver/Medium/0_1_knapsack_problem.cpp
+++ DP/DP_Series_Striver/Medium/0_1_knapsack_problem.cpp
@@ -10,34 +10,37 @@ For example, N = 4, W = 10 and the weights and values of items are weights = [6,
 and values = [3, 6, 1, 4]. Then the best way to fill the knapsack is to choose items with 
 weight 6, 1 and 3. The total value of knapsack = 3 + 6 + 4 = 13.
 ------------------------------------------------------------------------------
-// Function to solve the 0/1 Knapsack problem using memoization
-int knapsackUtil(vector<int>& wt, vector<int>& val, int ind, int W, vector<vector<int>>& dp) {
-    // Base case: If there are no items left or the knapsack has no capacity, return 0
-    if (ind == 0 || W == 0) {
-        return 0;
-    }
+int knapsackUtil(int W, int wt[], int val[], int index, vector<vector<int>>&dp)
+    {
+       //Base case:
+       if (index < 0) 
+       {
+            return 0;
+       }
 
+       
     // If the result for this state is already calculated, return it
-    if (dp[ind][W] != -1) {
-        return dp[ind][W];
+    if (dp[index][W] != -1) {
+        return dp[index][W];
     }
 
     // Calculate the maximum value by either excluding the current item or including it
-    int notTaken = knapsackUtil(wt, val, ind - 1, W, dp);
+    int notTaken = knapsackUtil(W, wt, val, index - 1, dp);
     int taken = 0;
 
     // Check if the current item can be included without exceeding the knapsack's capacity
-    if (wt[ind] <= W) {
-        taken = val[ind] + knapsackUtil(wt, val, ind - 1, W - wt[ind], dp);
+    if (wt[index] <= W) {
+        taken = val[index] + knapsackUtil(W - wt[index], wt, val, index - 1, dp);
     }
 
     // Store the result in the DP table and return
-    return dp[ind][W] = max(notTaken, taken);
-}
-
-
-int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
-{
-	vector<vector<int>> dp(n, vector<int>(w + 1, -1));
-    return knapsackUtil(weights, values, n-1, w, dp);
-}
+    return dp[index][W] = max(notTaken, taken);
+       
+    }
+    //Function to return max value that can be put in knapsack of capacity W.
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       vector<vector<int>>dp(n, vector<int>(W+1, -1));    
+       return knapsackUtil(W, wt, val, n-1, dp);
+    }
+    
