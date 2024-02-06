2905186bb72a16dbc0ba5777b40a46f12a83dbfa
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-06 13:05:52 +0530

    Update minimum_path_sum_grid.cpp

--- DP/Medium/Code_Practice/minimum_path_sum_grid.cpp
+++ DP/Medium/Code_Practice/minimum_path_sum_grid.cpp
@@ -1,29 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Helper function to find the minimum sum path from (i, j) to (0, 0) in the matrix
-int minSumPathUtil(int i, int j, vector<vector<int>> &matrix, vector<vector<int>> &dp) {
-    // Base cases
-    if (i == 0 && j == 0)
-        return matrix[0][0]; // If we are at the top-left corner, the minimum path sum is the value at (0, 0)
-    if (i < 0 || j < 0)
-        return INT_MAX; // If we go out of bounds, return a large value to avoid considering this path
-    if (dp[i][j] != -1)
-        return dp[i][j]; // If the result is already computed, return it
+int solve(vector<vector<int>> & dp, vector<vector<int>> &grid, int row, int col) {
+        if(row == 0 && col == 0) return grid[0][0];
+        if(row < 0 || col < 0) return INT_MAX;
+        if(dp[row][col] != -1) return dp[row][col];
 
-    // Calculate the minimum sum path by considering moving up and moving left
-    int up = matrix[i][j] + minSumPathUtil(i - 1, j, matrix, dp);
-    int left = matrix[i][j] + minSumPathUtil(i, j - 1, matrix, dp);
+        return dp[row][col] = grid[row][col] + min(solve(dp, grid, row - 1, col), solve(dp, grid, row, col - 1));
 
-    // Store the result in the DP table and return it
-    return dp[i][j] = min(up, left);
-}
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        int ans = 0;
+        int m = grid.size();
+        int n = grid[0].size();
 
-// Main function to find the minimum sum path in the matrix
-int minSumPath(int n, int m, vector<vector<int>> &matrix) {
-    vector<vector<int>> dp(n, vector<int>(m, -1)); // DP table to memoize results
-    return minSumPathUtil(n - 1, m - 1, matrix, dp); // Start from the bottom-right corner
-}
+        vector<vector<int>> dp(m , vector<int> (n , -1));
+
+        return solve(dp, grid, m-1, n-1);
+
+    }
 
 int main() {
     vector<vector<int>> matrix{
