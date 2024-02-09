4cbf380477281c678a49674efc5fb19cf39ffab7
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-09 15:51:06 +0530

    Create longest_consecutive_sequence.cpp

---
+++ Array/Hard/longest_consecutive_sequence.cpp
@@ -0,0 +1,54 @@
+Problem Statement: You are given an array of ‘N’ integers. You need to find the length of the longest
+sequence which contains the consecutive elements.
+Example 1:
+Input: [100, 200, 1, 3, 2, 4]
+Output: 4
+Explanation: The longest consecutive subsequence is 1, 2, 3, and 4.
+
+Input: [3, 8, 5, 7, 6]
+Output: 4
+Explanation: The longest consecutive subsequence is 5, 6, 7, and 8.
+-----------------------------------------------------------------------  
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int longestSuccessiveElements(vector<int>&a) {
+    int n = a.size();
+    if (n == 0) return 0;
+
+    int longest = 1;
+    unordered_set<int> st;
+    //put all the array elements into set:
+    for (int i = 0; i < n; i++) {
+        st.insert(a[i]);
+    }
+
+    //Find the longest sequence:
+    for (auto it : st) {
+        //if 'it' is a starting number:
+        if (st.find(it - 1) == st.end()) {
+            //find consecutive numbers:
+            int cnt = 1;
+            int x = it;
+            while (st.find(x + 1) != st.end()) {
+                x = x + 1;
+                cnt = cnt + 1;
+            }
+            longest = max(longest, cnt);
+        }
+    }
+    return longest;
+
+}
+
+int main()
+{
+    vector<int> a = {100, 200, 1, 2, 3, 4};
+    int ans = longestSuccessiveElements(a);
+    cout << "The longest consecutive sequence is " << ans << "\n";
+    return 0;
+}
+  
