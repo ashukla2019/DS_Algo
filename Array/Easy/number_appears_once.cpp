d56e006a4892269e8eae4fc58fe16c0c0637a768
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-09 12:55:21 +0530

    Create number_appears_once.cpp

---
+++ Array/Easy/number_appears_once.cpp
@@ -0,0 +1,33 @@
+Example 1:
+Input Format: arr[] = {2,2,1}
+Result: 1
+Explanation: In this array, only the element 1 appear once and so it is the answer.
+
+Example 2:
+Input Format: arr[] = {4,1,2,1,2}
+Result: 4
+Explanation: In this array, only element 4 appear once and the other elements appear twice. 
+So, 4 is the answer.
+  ------------------------------------------------------------------------- 
+  Approach:
+Assume the given array is: [4,1,2,1,2]
+XOR of all elements = 4^1^2^1^2
+      = 4 ^ (1^1) ^ (2^2)
+      = 4 ^ 0 ^ 0 = 4^0 = 4
+Hence, 4 is the single element in the array.
+-------------------------------------------------------------------------------
+int getSingleElement(vector<int> &arr) 
+{
+    //size of the array:
+    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}  
+Time Complexity: O(N), where N = size of the array.
+Reason: We are iterating the array only once.
+Space Complexity: O(1) as we are not using any extra space.
