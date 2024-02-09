9a09076c0b491cb75cb45018e38c6cfb0db068b7
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-09 11:28:48 +0530

    Create rotate_array_by_k_elements.cpp

---
+++ Array/Easy/rotate_array_by_k_elements.cpp
@@ -0,0 +1,33 @@
+Example 1:
+Input: N = 7, array[] = {1,2,3,4,5,6,7} , k=2 , right
+Output: 6 7 1 2 3 4 5
+Explanation: array is rotated to right by 2 position .
+
+Example 2:
+Input: N = 6, array[] = {3,7,8,9,10,11} , k=3 , left 
+Output: 9 10 11 3 7 8
+Explanation: Array is rotated to right by 3 position.
+----------------------------------------------------------------------------------  
+void Reverse(int arr[], int start, int end)
+{
+  while (start <= end)
+  {
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+}
+// Function to Rotate k elements to right
+void Rotateeletoright(int arr[], int n, int k)
+{
+  // Reverse first n-k elements
+  Reverse(arr, 0, n - k - 1);
+  // Reverse last k elements
+  Reverse(arr, n - k, n - 1);
+  // Reverse whole array
+  Reverse(arr, 0, n - 1);
+}  
+Time Complexity – O(N) where N is the number of elements in an array
+Space Complexity – O(1) since no extra space is required
