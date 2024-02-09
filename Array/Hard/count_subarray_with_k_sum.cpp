ad45c07fa7b82f7654a2d164cd9b64f3639b4a9a
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-09 15:56:46 +0530

    Create count_subarray_with_k_sum.cpp

---
+++ Array/Hard/count_subarray_with_k_sum.cpp
@@ -0,0 +1,50 @@
+Problem Statement: Given an array of integers and an integer k, return the total number of
+subarrays whose sum equals k.
+A subarray is a contiguous non-empty sequence of elements within an array.
+Example 1:
+Input Format: N = 4, array[] = {3, 1, 2, 4}, k = 6
+Result: 2
+Explanation: The subarrays that sum up to 6 are [3, 1, 2] and [2, 4].
+
+Example 2:
+Input Format: N = 3, array[] = {1,2,3}, k = 3
+Result: 2
+Explanation: The subarrays that sum up to 3 are [1, 2], and [3].
+---------------------------------------------------------------------  
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    int n = arr.size(); // size of the given array.
+    map mpp;
+    int preSum = 0, cnt = 0;
+
+    mpp[0] = 1; // Setting 0 in the map.
+    for (int i = 0; i < n; i++) {
+        // add current element to prefix Sum:
+        preSum += arr[i];
+
+        // Calculate x-k:
+        int remove = preSum - k;
+
+        // Add the number of subarrays to be removed:
+        cnt += mpp[remove];
+
+        // Update the count of prefix sum
+        // in the map.
+        mpp[preSum] += 1;
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector arr = {3, 1, 2, 4};
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
+
+  
