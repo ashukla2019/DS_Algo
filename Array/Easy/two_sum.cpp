675159820b9d5019b64da9fdbdd134658cd14b4d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-09 13:02:02 +0530

    Create two_sum.cpp

---
+++ Array/Easy/two_sum.cpp
@@ -0,0 +1,33 @@
+Problem Statement: Given an array of integers arr[] and an integer target.
+1st variant: Return YES if there exist two numbers such that their sum is equal to the target. 
+Otherwise, return NO.
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. 
+Otherwise, we will return {-1, -1}.
+Example 1:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 14
+Result: YES (for 1st variant)
+       [1, 3] (for 2nd variant)
+Explanation: arr[1] + arr[3] = 14. So, the answer is “YES” for the first variant and [1, 3] for 2nd variant.
+
+Example 2:
+Input Format: N = 5, arr[] = {2,6,5,8,11}, target = 15
+Result: NO (for 1st variant)
+	[-1, -1] (for 2nd variant)
+Explanation: There exist no such two numbers whose sum is equal to the target.
+------------------------------------------------------------------------------------------- 
+string twoSum(int n, vector<int> &arr, int target) {
+    sort(arr.begin(), arr.end());
+    int left = 0, right = n - 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == target) {
+            return "YES";
+        }
+        else if (sum < target) left++;
+        else right--;
+    }
+    return "NO";
+}
+Time Complexity: O(N) + O(N*logN), where N = size of the array.
+Reason: The loop will run at most N times. And sorting the array will take N*logN time complexity.
+Space Complexity: O(1) as we are not using any extra space.
