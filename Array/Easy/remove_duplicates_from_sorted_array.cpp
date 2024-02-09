5b6c9ae978f59c8f53858505402b755fb1914ea6
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-09 12:39:24 +0530

    Create remove_duplicates_from_sorted_array.cpp

---
+++ Array/Easy/remove_duplicates_from_sorted_array.cpp
@@ -0,0 +1,29 @@
+Example 1: 
+
+Input: arr[1,1,2,2,2,3,3]
+
+Output: arr[1,2,3,_,_,_,_]
+
+Explanation: Total number of unique elements are 3, i.e[1,2,3] and Therefore return 3 after assigning [1,2,3] in the beginning of the array.
+
+Example 2: 
+
+Input: arr[1,1,1,2,2,3,3,3,3,4,4]
+
+Output: arr[1,2,3,4,_,_,_,_,_,_,_]
+
+Explanation: Total number of unique elements are 4, i.e[1,2,3,4] and Therefore return 4 after assigning [1,2,3,4] in the beginning of the array.
+--------------------------------------------------------------------------------------------  
+int removeDuplicates(int arr[], int n)
+{
+  int i = 0;
+  for (int j = 1; j < n; j++) {
+    if (arr[i] != arr[j]) {
+      i++;
+      arr[i] = arr[j];
+    }
+  }
+  return i + 1;
+}
+Time Complexity: O(N)
+Space Complexity: O(1)
