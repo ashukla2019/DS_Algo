c3785fc22faf738ea67abc0f3ba5dd659d1fa80c
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-11 13:19:30 +0530

    Update secondLargest_Smallest_element.cpp

--- Array/Basic/secondLargest_Smallest_element.cpp
+++ Array/Basic/secondLargest_Smallest_element.cpp
@@ -12,5 +12,33 @@ Second Largest : -1
 Explanation: Since there is only one element in the array, it is the largest and smallest 
 element present in the array. There is no second largest or second smallest element present.
 ---------------------------------------------------------------------------------------------- 
+ int findSecondSmallestElement(int arr[])
+{
+	int n = sizeof(arr) / sizeof(arr[0]); 
+    int smallest = INT_MAX; 
+    // traversing the array to find 
+    // smallest element. 
+    for (int i = 0; i < n; i++) 
+    { 
+        if (arr[i] < smallest) 
+        { 
+            smallest = arr[i]; 
+        } 
+    } 
+    cout << "smallest element is: " << smallest << endl; 
   
+    int second_smallest = INT_MAX; 
+  
+    // traversing the array to find second smallest element 
+    for (int i = 0; i < n; i++) 
+    { 
+        if (arr[i] < second_smallest && arr[i] > smallest) 
+        { 
+            second_smallest = arr[i]; 
+        } 
+    } 
+    return second_smallest;
+}
+Time Complexity: O(n)
+Auxiliary Space: O(1)
   
