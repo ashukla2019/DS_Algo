81795e72c341bd8b37a186ad5390b7f2632e8a31
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-11 13:32:08 +0530

    Update move_all_zeros_to_end_array.cpp

--- Array/Easy/move_all_zeros_to_end_array.cpp
+++ Array/Easy/move_all_zeros_to_end_array.cpp
@@ -8,29 +8,22 @@ Input: 1,2,0,1,0,4,0
 Output: 1,2,1,4,0,0,0
 Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
 ------------------------------------------------------------------------------------------- 
-vector<int> moveZeros(int n, vector<int> a) {
-    int j = -1;
-    //place the pointer j:
-    for (int i = 0; i < n; i++) {
-        if (a[i] == 0) {
-            j = i;
-            break;
-        }
+vector<int> moveZeros(int n, vector<int> a) 
+{
+    int count=0;
+    for(int i=0; i<n; j++)
+    {
+    	if(a[i] !=0) //Checking non_zero element and storing in array with new index count.
+    	{
+			arr[count++]=arr[i];
+    	}
     }
-
-    //no non-zero elements:
-    if (j == -1) return a;
-
-    //Move the pointers i and j
-    //and swap accordingly:
-    for (int i = j + 1; i < n; i++) {
-        if (a[i] != 0) {
-            swap(a[i], a[j]);
-            j++;
-        }
+    while(count<n) //count upto n value will have zeros so store them
+    {
+    	a[count++] = 0;
     }
     return a;
+    
 }
 Time Complexity: O(N), N = size of the array.
-Reason: We have used 2 loops and using those loops, we are basically traversing the array once.
 Space Complexity: O(1) as we are not using any extra space to solve this problem.  
