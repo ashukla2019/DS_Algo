df6de22169c875dbe3b400e4f8c61a9c26e4e856
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-15 15:19:41 +0530

    Update Remove_N-th_node_from_end.cpp

--- Linked_list/linked_list/Medium/Code_Practice/Remove_N-th_node_from_end.cpp
+++ Linked_list/linked_list/Medium/Code_Practice/Remove_N-th_node_from_end.cpp
@@ -1,3 +1,12 @@
+Input:
+N = 2
+LinkedList: 1->2->3->4->5->6->7->8->9
+Output: 8
+Explanation: In the first example, there
+are 9 nodes in linked list and we need
+to find 2nd node from end. 2nd node
+from end is 8.  
+-----------------------------------------------------------------------------------------------
 /*
 1) Two pointers first and second will be set at head node
 2) first will traverse n distance(1 to n), first will be ahead of second by n step
