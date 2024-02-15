1bef7784f29b7a9c570fcdf3e32e3f93bcaa51b7
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-15 15:10:45 +0530

    Update Add_Two_numbers.cpp

--- Linked_list/linked_list/Medium/Code_Practice/Add_Two_numbers.cpp
+++ Linked_list/linked_list/Medium/Code_Practice/Add_Two_numbers.cpp
@@ -1,3 +1,12 @@
+Problem: You are given two non-empty linked lists representing two non-negative integers. The digits are
+stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return 
+the sum as a linked list.
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+        2---->4---->3
+        5---->6---->4
+result= 7---->0---->8    
+
 /*
 1) Create dummy node with val 0
 2) Set result to dummy
