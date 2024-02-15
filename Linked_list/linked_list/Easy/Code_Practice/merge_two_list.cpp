c55a66d26aae116c4a789a97ae79fbe10fcc7c26
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-15 12:35:07 +0530

    Update merge_two_list.cpp

--- Linked_list/linked_list/Easy/Code_Practice/merge_two_list.cpp
+++ Linked_list/linked_list/Easy/Code_Practice/merge_two_list.cpp
@@ -1,3 +1,11 @@
+Problem:
+You are given the heads of two sorted linked lists list1 and list2.
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes
+of the first two lists.
+1---->2----->4 (l1)
+1---->3----->4 (l2) 
+result: 1-->1-->2-->3-->4-->4 (l3= l1+l2)   
+Return the head of the merged linked list.
 /*
 1) Create result linked list
 2) Check if any of list is empty/null then return other(if l1 empty then return l2, viceversa)
@@ -10,7 +18,7 @@
 Time Complexity: O(n)
 Space Complexity: O(n) -> we have used new linked list to store data.
  */ 
-
+--------------------------------------------------------------------------------------
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) 
     {
         ListNode* result;
