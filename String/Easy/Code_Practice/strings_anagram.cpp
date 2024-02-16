8630cc7abd8560d23e22f00902aa7c874cea094d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-16 14:32:22 +0530

    Create strings_anagram.cpp

---
+++ String/Easy/Code_Practice/strings_anagram.cpp
@@ -0,0 +1,58 @@
+Given two strings. The task is to check whether the given strings are anagrams of each other or not. 
+An anagram of a string is another string that contains the same characters, only the order of characters 
+can be different. For example, “abcd” and “dabc” are an anagram of each other.
+
+Examples:
+Input: str1 = “listen”  str2 = “silent”
+Output: “Anagram”
+Explanation: All characters of “listen” and “silent” are the same.
+
+Input: str1 = “gram”  str2 = “arm”
+Output: “Not Anagram”
+---------------------------------------------------------------------------------------- 
+// C++ program to check whether two strings are anagrams
+// of each other
+#include <bits/stdc++.h>
+using namespace std;
+
+/* function to check whether two strings are anagram of
+each other */
+bool areAnagram(string str1, string str2)
+{
+	// Get lengths of both strings
+	int n1 = str1.length();
+	int n2 = str2.length();
+
+	// If length of both strings is not same, then they
+	// cannot be anagram
+	if (n1 != n2)
+		return false;
+
+	// Sort both the strings
+	sort(str1.begin(), str1.end());
+	sort(str2.begin(), str2.end());
+
+	// Compare sorted strings
+	for (int i = 0; i < n1; i++)
+		if (str1[i] != str2[i])
+			return false;
+
+	return true;
+}
+
+// Driver code
+int main()
+{
+	string str1 = "gram";
+	string str2 = "arm";
+
+	// Function Call
+	if (areAnagram(str1, str2))
+		cout << "The two strings are anagram of each other";
+	else
+		cout << "The two strings are not anagram of each "
+				"other";
+
+	return 0;
+}
+  
