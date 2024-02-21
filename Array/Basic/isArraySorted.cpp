45b1f479816a0beea0246701e391d431604abe49
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-21 18:24:46 +0530

    Update isArraySorted.cpp

--- Array/Basic/isArraySorted.cpp
+++ Array/Basic/isArraySorted.cpp
@@ -6,9 +6,9 @@ equals to its next values, So the answer is True.
 ----------------------------------------------------------------------------   
   bool isSorted(int arr[], int n) 
 {
-  for (int i = 1; i < n; i++) 
+  for (int i = 0; i < n-1; i++) 
   {
-    if (arr[i] < arr[i - 1])
+    if (arr[i] < arr[i + 1])
       return false;
   }
 
