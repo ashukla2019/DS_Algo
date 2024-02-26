41b519983a5d2a33bf4896df89c6da9d68a16830
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-02-26 12:07:54 +0530

    Update detect_cycle_undirected_graph_bfs.cpp

--- Graph/Medium/Code_Practice/detect_cycle_undirected_graph_bfs.cpp
+++ Graph/Medium/Code_Practice/detect_cycle_undirected_graph_bfs.cpp
@@ -1,3 +1,10 @@
+The algorithm steps are as follows:
+For BFS traversal, we need a queue data structure and a visited array. 
+Push the pair of the source node and its parent data (<source, parent>) in the queue, and mark the node as visited. The parent will be needed so that we don’t do a backward traversal in the graph, we just move frontwards. 
+Start the BFS traversal, pop out an element from the queue every time and travel to all its unvisited neighbors using an adjacency list.
+Repeat the steps either until the queue becomes empty, or a node appears to be already visited which is not the parent, even though we traveled in different directions during the traversal, indicating there is a cycle.
+If the queue becomes empty and no such node is found then there is no cycle in the graph.
+-----------------------------------------------------------------------------------
 bool detect(int src, vector<int> adj[], int vis[]) {
       vis[src] = 1; 
       // store <source node, parent node>
