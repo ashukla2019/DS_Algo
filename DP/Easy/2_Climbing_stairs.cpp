d698132e813cfe6eaf9a4bec187112db58e56f2d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-12 17:49:19 +0530

    Update 2_Climbing_stairs.cpp

--- DP/Easy/2_Climbing_stairs.cpp
+++ DP/Easy/2_Climbing_stairs.cpp
@@ -1,6 +1,6 @@
-Problem: There are n stairs, a person standing at the bottom wants to climb stairs to reach the nth stair.
-The person can climb either 1 stair or 2 stairs at a time, the task is to count the number of ways 
-that a person can reach at the top.
+Problem Statement: Given a number of stairs. Starting from the 0th stair we need to climb to the
+“Nth” stair. At a time we can climb either one or two steps. We need to return the total number 
+of distinct ways to reach from 0th to Nth stair.
 
 //Observation: We have to find all distinct ways to reach to top stair.
 Recurrence releation:
@@ -20,7 +20,7 @@ int climbStairs(int n)
 //Memorization
 int climbStairs(int n) 
 {
-	vector<int>dp(n+1, -1);
+	vector<int>dp(n+1, -1); //steps 0->n(n+1)
 	//Base case: 
 	//if n=1, there is only one way(1 step move) and n=0 then there is only one way.
 	if(n<=1)
