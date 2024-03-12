34f46531755140ed553d344808a67ee2b580713a
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-12 17:28:10 +0530

    Update 2_Climbing_stairs.cpp

--- DP/Easy/2_Climbing_stairs.cpp
+++ DP/Easy/2_Climbing_stairs.cpp
@@ -1,3 +1,7 @@
+Problem: There are n stairs, a person standing at the bottom wants to climb stairs to reach the nth stair.
+The person can climb either 1 stair or 2 stairs at a time, the task is to count the number of ways 
+that a person can reach at the top.
+
 //Observation: We have to find all distinct ways to reach to top stair.
 Recurrence releation:
 f(n)=f(n-1)+f(n-2)
