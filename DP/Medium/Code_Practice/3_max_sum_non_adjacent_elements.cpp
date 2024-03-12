537461b209b95ae21e06c73cefc4b0d27e601b7d
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-12 22:21:21 +0530

    Update 3_max_sum_non_adjacent_elements.cpp

--- DP/Medium/Code_Practice/3_max_sum_non_adjacent_elements.cpp
+++ DP/Medium/Code_Practice/3_max_sum_non_adjacent_elements.cpp
@@ -1,3 +1,20 @@
+Problem statement
+You are given an array/list of ‘N’ integers. You are supposed to return the
+maximum sum of the subsequence with the constraint that no two elements are adjacent
+in the given array/list.
+Ex: 
+Input: arr[] = {5, 5, 10, 100, 10, 5}
+Output: 110
+Explanation: Pick the subsequence {5, 100, 5}.
+The sum is 110 and no two elements are adjacent. This is the highest possible sum.
+
+Input: arr[] = {3, 2, 7, 10}
+Output: 13
+Explanation: The subsequence is {3, 10}. This gives sum = 13.    
+
+Note:
+A subsequence of an array/list is obtained by deleting some number of elements (can be zero) from the array/list, leaving the remaining elements in their original order.
+---------------------------------------------------------------------------------
 // Function to solve the problem using dynamic programming
 int solveUtil(int ind, vector<int>& arr, vector<int>& dp) {
     // If the result for this index is already computed, return it
