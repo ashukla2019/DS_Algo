329ae5b44d18af431e573bee45e2e06f68ad11e8
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-12 11:29:40 +0530

    Update 2_sum.cpp

--- Array/Easy/2_sum.cpp
+++ Array/Easy/2_sum.cpp
@@ -31,3 +31,33 @@ string twoSum(int n, vector<int> &arr, int target) {
 Time Complexity: O(N) + O(N*logN), where N = size of the array.
 Reason: The loop will run at most N times. And sorting the array will take N*logN time complexity.
 Space Complexity: O(1) as we are not using any extra space.
+-------------------------------------------------------------------------------
+2nd variant: Return indices of the two numbers such that their sum is equal to the target. 
+Otherwise, we will return {-1, -1}.
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(int n, vector<int> &arr, int target) {
+    unordered_map<int, int> mpp;
+    for (int i = 0; i < n; i++) {
+        int num = arr[i];
+        int moreNeeded = target - num;
+        if (mpp.find(moreNeeded) != mpp.end()) {
+            return {mpp[moreNeeded], i};
+        }
+        mpp[num] = i;
+    }
+    return { -1, -1};
+}
+
+int main()
+{
+    int n = 5;
+    vector<int> arr = {2, 6, 5, 8, 11};
+    int target = 14;
+    vector<int> ans = twoSum(n, arr, target);
+    cout << "This is the answer for variant 2: [" << ans[0] << ", "
+         << ans[1] << "]" << endl;
+    return 0;
+}
+	
