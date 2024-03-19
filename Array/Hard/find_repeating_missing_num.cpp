c063528ced5db76213b2b6194a1ad14bcbe3c5ee
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-03-19 14:02:23 +0530

    Create find_repeating_missing_num.cpp

---
+++ Array/Hard/find_repeating_missing_num.cpp
@@ -0,0 +1,47 @@
+Problem Statement: You are given a read-only array of N integers with values also in the range [1, N]
+both inclusive. Each integer appears exactly once except A which appears twice and B which is missing.
+The task is to find the repeating and missing numbers A and B where A repeats twice and B is missing.
+Example 1:
+Input Format:  array[] = {3,1,2,5,3}
+Result: {3,4)
+Explanation: A = 3 , B = 4 
+Since 3 is appearing twice and 4 is missing
+
+Example 2:
+Input Format: array[] = {3,1,2,5,4,6,7,5}
+Result: {5,8)
+Explanation: A = 5 , B = 8 
+Since 5 is appearing twice and 8 is missing
+---------------------------------------------------------------------
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findMissingRepeatingNumbers(vector<int> a) {
+    int n = a.size(); // size of the array
+    int hash[n + 1] = {0}; // hash array
+
+    //update the hash array:
+    for (int i = 0; i < n; i++) {
+        hash[a[i]]++;
+    }
+
+    //Find the repeating and missing number:
+    int repeating = -1, missing = -1;
+    for (int i = 1; i <= n; i++) {
+        if (hash[i] == 2) repeating = i;
+        else if (hash[i] == 0) missing = i;
+
+        if (repeating != -1 && missing != -1)
+            break;
+    }
+    return {repeating, missing};
+}
+
+int main()
+{
+    vector<int> a = {3, 1, 2, 5, 4, 6, 7, 5};
+    vector<int> ans = findMissingRepeatingNumbers(a);
+    cout << "The repeating and missing numbers are: {"
+         << ans[0] << ", " << ans[1] << "}\n";
+    return 0;
+}
