31e27f17b12a78bf00aaa9c14b87dbf49c7b640f
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-01 13:56:16 +0530

    Update 6_bipartite_graph_bfs.cpp

--- Graph/Medium/Code_Practice/6_bipartite_graph_bfs.cpp
+++ Graph/Medium/Code_Practice/6_bipartite_graph_bfs.cpp
@@ -1,71 +1,81 @@
-#include<bits/stdc++.h>
-using namespace std;
+Problem statement
+Given a graph, check whether the graph is bipartite or not. Your function should return true if the given graph's vertices
+can be divided into two independent sets, ‘U’ and ‘V’ such that every edge (‘u’, ‘v’) either connects a vertex from ‘U’ to ‘V’
+or a vertex from ‘V’ to ‘U’.
 
-class Solution {
-    // colors a component 
-    private: 
-    bool check(int start, int V, vector<int>adj[], int color[]) {
-        queue<int> q;
-	    q.push(start); 
-	    color[start] = 0; 
-	    while(!q.empty()) {
-	        int node = q.front();
-	        q.pop(); 
-	        
-	        for(auto it : adj[node]) {
-	            // if the adjacent node is yet not colored
-	            // you will give the opposite color of the node 
-	            if(color[it] == -1) {
-	                
-	                color[it] = !color[node]; 
-	                q.push(it); 
-	            }
-	            // is the adjacent guy having the same color 
-	            // someone did color it on some other path 
-	            else if(color[it] == color[node]) {
-	                return false; 
-	            }
-	        }
-	    }
-	    return true; 
-    }
-public:
-	bool isBipartite(int V, vector<int>adj[]){
-	    int color[V]; 
-	    for(int i = 0;i<V;i++) color[i] = -1; 
-	    
-	    for(int i = 0;i<V;i++) {
-	        // if not coloured
-	        if(color[i] == -1) {
-	            if(check(i, V, adj, color) == false) {
-	                return false; 
-	            }
-	        }
-	    }
-	    return true; 
+You are given a 2D array ‘edges’ which contains 0 and 1, where ‘edges[i][j]’ = 1 denotes a bi-directional edge from ‘i’ to ‘j’.
+Note:
+If edges[i][j] = 1, that implies there is a bi-directional edge between ‘i’ and ‘j’, that means there exists both edges from
+‘i’ to ‘j’ and to ‘j’ to ‘i’.
+For example
+
+Given:
+‘N’ = 3
+‘edges’ = [[0, 1, 1], [0, 0, 1], [0,0,0]]. 
+
+Sample Input 1 :
+2
+4
+0 1 0 0 
+0 0 0 1 
+0 0 0 0 
+0 0 0 0 
+3
+0 1 1
+0 0 1
+0 0 0
+Sample Output 1 :
+1
+0 	
+	
+#include <bits/stdc++.h>
+bool check(int start, vector<int>adj[], int color[])
+{
+	queue<int>q;
+	q.push(start);
+	color[start] = 0;
+
+	while(!q.empty())
+	{
+		int n = q.front();
+		q.pop();
+		for(auto adjnode:adj[n])
+		{
+			if(color[adjnode] == -1)
+			{
+				color[adjnode] = !color[n];
+				q.push(adjnode);
+			}
+			else if(color[adjnode] == color[n])
+			    return false;
+		}
 	}
-
-};
-
-void addEdge(vector <int> adj[], int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+	return true;
 }
+bool isGraphBirpatite(vector<vector<int>> &edges) {
+	int V = edges.size();
+	vector<int> adj[V];
+	for(int i = 0; i < V; i++)
+	{
+		for(int j = 0; j < V; j++)
+		{
+			if(edges[i][j] == 1 && i != j)
+			{
+				adj[i].push_back(j);
+				adj[j].push_back(i);
+			}
+		}
+	}
+	int color[V];
+	for(int i = 0; i < V; i++)  color[i]=-1;
 
-int main(){
-	
-	// V = 4, E = 4
-	vector<int>adj[4];
-	
-	addEdge(adj, 0, 2);
-   	addEdge(adj, 0, 3);
-    	addEdge(adj, 2, 3);
-    	addEdge(adj, 3, 1);
-
-	Solution obj;
-	bool ans = obj.isBipartite(4, adj);    
-	if(ans)cout << "1\n";
-	else cout << "0\n";  
-	
-	return 0;
+	for(int i = 0; i < V; i++)
+	{
+		if(color[i] == -1)
+		{
+			if(check(i,adj,color) == false)
+			    return false;
+		}
+	}
+	return true;
 }
