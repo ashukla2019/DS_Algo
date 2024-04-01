6df184d07096a7fac259270a09733075d9d0d0a2
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-01 11:48:54 +0530

    Update dfs_graph.cpp

--- Graph/Easy/Code_Practice/dfs_graph.cpp
+++ Graph/Easy/Code_Practice/dfs_graph.cpp
@@ -56,3 +56,65 @@ int main()
 
     return 0;
 }
+
+-----------------------------------------------------------------------------------------------------------------------------  
+Problem statement
+Given an undirected and disconnected graph G(V, E), containing 'V' vertices and 'E' edges, the information about edges is given 
+using 'GRAPH' matrix, where i-th edge is between GRAPH[i][0] and GRAPH[i][1]. print its DFS traversal.
+V is the number of vertices present in graph G and vertices are numbered from 0 to V-1.   
+
+E is the number of edges present in graph G.
+Note : The Graph may not be connected i.e there may exist multiple components in a graph.    
+
+Sample Input 1:
+5 4
+0 2
+0 1
+1 2
+3 4
+Sample Output 1:
+2
+0 1 2
+3 4
+
+void dfs(int start, vector<vector<int>>&adj,vector<int>&a, vector<int>&vis)
+{
+    vis[start]=1;
+    a.push_back(start);
+    for(auto it: adj[start])
+    {
+        if(vis[it]!= 1)
+        {
+            dfs(it, adj,a, vis);
+        }
+    }
+}
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{
+    // Adjacency Matrix to Adjacency list conversion
+      vector<vector<int>> adj(V);
+      for(int i=0; i<edges.size(); i++)
+      {
+             int u = edges[i][0];
+             int v = edges[i][1];
+             adj[u].push_back(v);
+             adj[v].push_back(u);
+      }
+
+       // Visited Array
+       vector<int> vis(V,0);
+       vector<vector<int>> ans;
+
+      // Traversing
+       for(int i=0; i<V; i++)
+       { 
+           if(vis[i]!=1)
+           {
+               vector<int> a;
+               dfs(i,adj,a,vis);
+               ans.push_back(a);
+           }
+       }
+       return ans;
+}      
