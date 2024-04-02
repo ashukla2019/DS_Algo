f70cadf9da0985940f51147f9a2e455a82add997
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-02 16:07:11 +0530

    Update 6_topological_sort_dfs.cpp

--- Graph/Medium/Code_Practice/6_topological_sort_dfs.cpp
+++ Graph/Medium/Code_Practice/6_topological_sort_dfs.cpp
@@ -1,50 +1,54 @@
-#include <bits/stdc++.h>
-using namespace std;
+Problem statement
+A Directed Acyclic Graph (DAG) is a directed graph that contains no cycles.
+Topological Sorting of DAG is a linear ordering of vertices such that for every directed edge from vertex ‘u’ to vertex ‘v’,
+vertex ‘u’ comes before ‘v’ in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+Given a DAG consisting of ‘V’ vertices and ‘E’ edges, you need to find out any topological sorting of this DAG. 
+Return an array of size ‘V’ representing the topological sort of the vertices of the given DAG.
 
-class Solution {
-private:
-	void dfs(int node, int vis[], stack<int> &st,
-	         vector<int> adj[]) {
-		vis[node] = 1;
-		for (auto it : adj[node]) {
-			if (!vis[it]) dfs(it, vis, st, adj);
-		}
-		st.push(node);
-	}
-public:
-	//Function to return list containing vertices in Topological order.
-	vector<int> topoSort(int V, vector<int> adj[])
-	{
-		int vis[V] = {0};
-		stack<int> st;
-		for (int i = 0; i < V; i++) {
-			if (!vis[i]) {
-				dfs(i, vis, st, adj);
-			}
-		}
+Sample Input 1:
+2
+2 1
+1 0
+3 2
+0 1
+0 2
+Sample Output 1:
+1 0
+0 2 1
 
-		vector<int> ans;
-		while (!st.empty()) {
-			ans.push_back(st.top());
-			st.pop();
-		}
-		return ans;
-	}
-};
+#include <bits/stdc++.h> 
+void dfs(int node, vector<int> adj[], int vis[], stack<int>&st)
+{
+    vis[node]=1;
 
-
-int main() {
-
-	//V = 6;
-	vector<int> adj[6] = {{}, {}, {3}, {1}, {0, 1}, {0, 2}};
-	int V = 6;
-	Solution obj;
-	vector<int> ans = obj.topoSort(V, adj);
-
-	for (auto node : ans) {
-		cout << node << " ";
-	}
-	cout << endl;
-
-	return 0;
+    for(auto it:adj[node])
+    {
+        if(!vis[it])
+        {
+            dfs(it,adj,vis,st);
+        }
+    }
+    st.push(node);
 }
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+    vector<int>adj[v];
+    for(auto it:edges)
+        adj[it[0]].push_back(it[1]);
+
+    int vis[v]={0};
+    stack<int>st;
+    for(int i = 0; i < v; i++)
+    {
+        if(!vis[i])
+        {
+            dfs(i,adj,vis,st);
+        }
+    }
+    vector<int>ans;
+    while(!st.empty())
+    {
+        ans.push_back(st.top());
+        st.pop();
+    }
+    return ans;
+}	
