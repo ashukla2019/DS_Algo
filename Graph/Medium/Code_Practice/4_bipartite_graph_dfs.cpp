a5d78fac375d982ce4ddf0b25d8b1052463461f1
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-02 15:39:54 +0530

    Update 4_bipartite_graph_dfs.cpp

--- Graph/Medium/Code_Practice/4_bipartite_graph_dfs.cpp
+++ Graph/Medium/Code_Practice/4_bipartite_graph_dfs.cpp
@@ -1,60 +1,79 @@
-#include<bits/stdc++.h>
-using namespace std;
+Problem:
+Given a graph, check whether the graph is bipartite or not. Your function should return true if the given graph's vertices 
+can be divided into two independent sets, ‘U’ and ‘V’ such that every edge (‘u’, ‘v’) either connects a vertex from ‘U’ to ‘V’
+or a vertex from ‘V’ to ‘U’.
 
-class Solution {
-private: 
-    bool dfs(int node, int col, int color[], vector<int> adj[]) {
-        color[node] = col; 
-        
-        // traverse adjacent nodes
-        for(auto it : adj[node]) {
-            // if uncoloured
-            if(color[it] == -1) {
-                if(dfs(it, !col, color, adj) == false) return false; 
-            }
-            // if previously coloured and have the same colour
-            else if(color[it] == col) {
-                return false; 
-            }
-        }
-        
-        return true; 
-    }
-public:
-	bool isBipartite(int V, vector<int>adj[]){
-	    int color[V];
-	    for(int i = 0;i<V;i++) color[i] = -1; 
-	    
-	    // for connected components
-	    for(int i = 0;i<V;i++) {
-	        if(color[i] == -1) {
-	            if(dfs(i, 0, color, adj) == false) 
-	                return false; 
-	        }
-	    }
-	    return true; 
+You are given a 2D array ‘edges’ which contains 0 and 1, where ‘edges[i][j]’ = 1 denotes a bi-directional edge from ‘i’ to ‘j’.
+Note:
+If edges[i][j] = 1, that implies there is a bi-directional edge between ‘i’ and ‘j’, that means there exists both edges from ‘i’ to ‘j’ and to ‘j’ to ‘i’.
+For example
+Given:
+‘N’ = 3
+‘edges’ = [[0, 1, 1], [0, 0, 1], [0,0,0]]. 
+
+Sample Input 1 :
+2
+4
+0 1 0 0 
+0 0 0 1 
+0 0 0 0 
+0 0 0 0 
+3
+0 1 1
+0 0 1
+0 0 0
+Sample Output 1 :
+1
+0 
+
+#include <bits/stdc++.h>
+bool dfs(int node, int col, int color[], vector<int>adj[])
+{
+	color[node]=col;
+	for(auto it : adj[node])
+	{
+		if(color[it]==-1)
+		{
+			if(dfs(it, !col, color, adj)==false) 
+				return false;
+		}
+		else if(color[it]==col){
+			return false;
+		}
 	}
-};
-
-void addEdge(vector <int> adj[], int u, int v) {
-    adj[u].push_back(v);
-    adj[v].push_back(u);
+	return true;
 }
 
-int main(){
-	
-	// V = 4, E = 4
-	vector<int>adj[4];
-	
-	addEdge(adj, 0, 2);
-   	addEdge(adj, 0, 3);
-	addEdge(adj, 2, 3);
-	addEdge(adj, 3, 1);
+bool isGraphBirpatite(vector<vector<int>> &edges) 
+{
+	//convert adjacency matrix to adjcency list
 
-	Solution obj;
-	bool ans = obj.isBipartite(4, adj);    
-	if(ans)cout << "1\n";
-	else cout << "0\n";  
+	int V= edges.size();
+	vector<int> adj[V];
+
+	for(int i=0;i<V;i++)
+	{
+		for(int j=0;j<V;j++)
+		{
+			if(edges[i][j]==1 && i!=j)
+			{
+				adj[i].push_back(j);
+				adj[j].push_back(i);
+			}
+		}
+	}
+
+	int color[V]; 
+	for(int i=0;i<V;i++) color[i]=-1; 
+
+	for(int i=0;i<V;i++)
+	{
+		if(color[i] == -1)
+		{
+			if(dfs(i, 0, color, adj)== false) 
+				return false;
+		}
+	}
+	return true;
+}	
 	
-	return 0;
-}
