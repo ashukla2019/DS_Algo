9a6243e8fabb944ab4f097d1da98142a33393dd6
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-02 15:52:53 +0530

    Update 5_topological_sort_bfs.cpp

--- Graph/Medium/Code_Practice/5_topological_sort_bfs.cpp
+++ Graph/Medium/Code_Practice/5_topological_sort_bfs.cpp
@@ -1,56 +1,62 @@
+A Directed Acyclic Graph (DAG) is a directed graph that contains no cycles.
+Topological Sorting of DAG is a linear ordering of vertices such that for every directed edge from vertex ‘u’ to vertex ‘v’,
+vertex ‘u’ comes before ‘v’ in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
 
-#include <bits/stdc++.h>
-using namespace std;
+Given a DAG consisting of ‘V’ vertices and ‘E’ edges, you need to find out any topological sorting of this DAG. 
+Return an array of size ‘V’ representing the topological sort of the vertices of the given DAG.
 
-class Solution {
-public:
-	//Function to return list containing vertices in Topological order.
-	vector<int> topoSort(int V, vector<int> adj[])
+#include <bits/stdc++.h> 
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  
+{
+    // Write your code here
+    vector<int> adj[v];
+
+    for(int i=0;i<e;i++)
 	{
-		int indegree[V] = {0};
-		for (int i = 0; i < V; i++) {
-			for (auto it : adj[i]) {
-				indegree[it]++;
-			}
-		}
+        adj[edges[i][0]].push_back(edges[i][1]); //directed graph
+    }
 
-		queue<int> q;
-		for (int i = 0; i < V; i++) {
-			if (indegree[i] == 0) {
-				q.push(i);
-			}
-		}
-		vector<int> topo;
-		while (!q.empty()) {
-			int node = q.front();
-			q.pop();
-			topo.push_back(node);
-			// node is in your topo sort
-			// so please remove it from the indegree
+    vector<int> inDegree(v,0);
+    //Find the indegree of all vertices
+    for(int i=0;i<v;i++)
+	{
+        for(auto it:adj[i])
+		{
+            inDegree[it]++;
+        }
+    }
 
-			for (auto it : adj[node]) {
-				indegree[it]--;
-				if (indegree[it] == 0) q.push(it);
-			}
-		}
+    queue<int> q;
 
-		return topo;
-	}
-};
+    // Push all the vertices with indegree 0 to queue
+    for(int i=0;i<v;i++)
+	{
+        if(inDegree[i]==0)
+		{
+            q.push(i);
+        }
+    }
+    vector<int> topo;
 
+    // Normal bfs traversal
+    while(!q.empty())
+	{
+        int f = q.front();
+        q.pop();
+        topo.push_back(f);
 
-int main() {
+        // For all the adjecent nodes of the cur node
+        for(auto it:adj[f])
+		{
+            //Each adjecent node will get visited hence decrease the indegree value.
+            inDegree[it]--;
+            if(inDegree[it]==0)
+			{
+                // Once indegree becomes zero push that node to queue.
+                q.push(it);
+            }
+        }
+    }
 
-	//V = 6;
-	vector<int> adj[6] = {{}, {}, {3}, {1}, {0, 1}, {0, 2}};
-	int V = 6;
-	Solution obj;
-	vector<int> ans = obj.topoSort(V, adj);
-
-	for (auto node : ans) {
-		cout << node << " ";
-	}
-	cout << endl;
-
-	return 0;
-}
+    return topo;
+}	
