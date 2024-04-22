aa022c1da3587a59829f5302638269943a8e80f3
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-22 12:14:57 +0530

    Create Top_K_frequent_element.cpp

---
+++ Hash_map_set/Medium/Top_K_frequent_element.cpp
@@ -0,0 +1,29 @@
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+Example 1:
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+  
+Example 2:
+Input: nums = [1], k = 1
+Output: [1]
+ 
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+----------------------------------------------------------------------------------------------------------------------  
+ vector<int> topKFrequent(vector<int>& nums, int k) {
+		unordered_map<int, int>m;
+		for(int i=0; i<nums.size(); i++){
+			m[nums[i]]++;
+		}
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>q;
+		for(auto it: m){
+			q.push({it.second, it.first});
+			if(q.size()>k){q.pop();}
+		}
+		vector<int>ans;
+		while(!q.empty()){
+			ans.push_back(q.top().second);
+			q.pop();
+		}
+		return ans;
+	} 
+  
