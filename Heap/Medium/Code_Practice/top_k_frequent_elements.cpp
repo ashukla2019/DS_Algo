cf229ba7e559f49846179a067d8803017b223275
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-22 15:58:19 +0530

    Update top_k_frequent_elements.cpp

--- Heap/Medium/Code_Practice/top_k_frequent_elements.cpp
+++ Heap/Medium/Code_Practice/top_k_frequent_elements.cpp
@@ -1,32 +1,29 @@
-Given an integer array nums and an integer k, return the k most frequent elements. 
-You may return the answer in any order.
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 Example 1:
 Input: nums = [1,1,1,2,2,3], k = 2
 Output: [1,2]
+  
 Example 2:
 Input: nums = [1], k = 1
 Output: [1]
-
-=----------------------------------------------------------------------------------
-vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> counts;
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> min_heap;
-        for(auto i : nums) {
-            counts[i]++;
-        }
-        
-        for(auto & i : counts) {
-            min_heap.push({i.second, i.first});
-            if(min_heap.size() > k) {
-                min_heap.pop();
-            }
-        }
-        
-        vector<int> res;
-         while(k--)
-        {
-            res.push_back(min_heap.top().second);
-            min_heap.pop();
-        }
-        return res;
-    }
+ 
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+----------------------------------------------------------------------------------------------------------------------  
+ vector<int> topKFrequent(vector<int>& nums, int k) {
+		unordered_map<int, int>m;
+		for(int i=0; i<nums.size(); i++){
+			m[nums[i]]++;
+		}
+		priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>>q;
+		for(auto it: m){
+			q.push({it.second, it.first});
+			if(q.size()>k){q.pop();}
+		}
+		vector<int>ans;
+		while(!q.empty()){
+			ans.push_back(q.top().second);
+			q.pop();
+		}
+		return ans;
+	} 
+  
