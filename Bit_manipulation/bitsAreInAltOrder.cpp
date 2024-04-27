81e5b43ca14a5543cb0419bf862ba00e77f61539
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-04-27 17:25:36 +0530

    Update bitsAreInAltOrder.cpp

--- Bit_manipulation/bitsAreInAltOrder.cpp
+++ Bit_manipulation/bitsAreInAltOrder.cpp
@@ -1,8 +1,38 @@
-// function to check if a number has bits in alternate pattern then all will be 1 
-//for n and n>>1. pattern: we know xor of 0 and 1 will be 1, if alternate bits are
-bool bitsAreInAltOrder(unsigned int n)
-{
-    unsigned int num = n ^ (n >> 1); 
-    // to check if all bits are set in 'num'
-    return allBitsAreSet(num);
-}
+// C++ implementation to check if a number 
+// has bits in alternate pattern 
+#include <bits/stdc++.h> 
+
+using namespace std; 
+
+// function to check if all the bits are set or not 
+// in the binary representation of 'n' 
+bool allBitsAreSet(unsigned int n) 
+{ 
+	// if true, then all bits are set 
+	if (((n + 1) & n) == 0) 
+		return true; 
+	// else all bits are not set 
+	return false; 
+} 
+
+// function to check if a number has bits in alternate 
+// pattern 
+bool bitsAreInAltOrder(unsigned int n) 
+{ 
+	unsigned int num = n ^ (n >> 1); 
+	// to check if all bits are set in 'num' 
+	return allBitsAreSet(num); 
+} 
+
+// Driver program to test above 
+int main() 
+{ 
+	unsigned int n = 10; 
+	if (bitsAreInAltOrder(n)) 
+		cout << "Yes"; 
+	else
+		cout << "No"; 
+	return 0; 
+} 
+
+// This code is contributed by Sania Kumari Gupta (kriSania804)
