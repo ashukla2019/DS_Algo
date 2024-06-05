0cfa789174a2ea60f33d78520e189e28da8c5028
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-06-05 17:16:54 +0530

    Update 4_insertion_sort.cpp

--- 3_Sorting/4_insertion_sort.cpp
+++ 3_Sorting/4_insertion_sort.cpp
@@ -1,37 +1,50 @@
-// C++ program for insertion sort
+Example 1:
+Input: N = 6, array[] = {13,46,24,52,20,9}
+Output: 9,13,20,24,46,52
+Explanation: 
+After sorting the array is: 9,13,20,24,46,52
+
+Example 2:
+Input: N=5, array[] = {5,4,3,2,1}
+Output: 1,2,3,4,5
+Explanation: After sorting the array is: 1,2,3,4,5
 
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to sort an array using
-// insertion sort
-void insertionSort(int arr[], int n)
+void insertion_sort(int arr[], int n) 
 {
-    int i, key, j;
-    for (i = 1; i < n; i++) {
-        key = arr[i];
-        j = i - 1;
-
-        // Move elements of arr[0..i-1],
-        // that are greater than key, 
-        // to one position ahead of their
-        // current position
-        while (j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j = j - 1;
+    for (int i = 0; i <= n - 1; i++) 
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j]) 
+        {
+            int temp = arr[j - 1];
+            arr[j - 1] = arr[j];
+            arr[j] = temp;
+            j--;
         }
-        arr[j + 1] = key;
     }
+
+    cout << "After Using insertion sort: " << "\n";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
 }
 
 int main()
 {
-    int arr[] = { 12, 11, 13, 5, 6 };
-    int N = sizeof(arr) / sizeof(arr[0]);
+    int arr[] = {13, 46, 24, 52, 20, 9};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Before Using insertion Sort: " << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
-    insertionSort(arr, N);
-    printArray(arr, N);
-
+    insertion_sort(arr, n);
     return 0;
 }
-
+Time complexity: O(N2)
