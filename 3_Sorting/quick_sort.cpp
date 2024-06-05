cf8cc0775394f654d1cca3cd315b8ca46cce17aa
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-06-05 14:15:37 +0530

    Update quick_sort.cpp

--- 3_Sorting/quick_sort.cpp
+++ 3_Sorting/quick_sort.cpp
@@ -3,6 +3,17 @@
 #include <iostream>
 using namespace std;
 
+//Algo steps:
+1. Choose the appropriate pivot element.
+2. Take two variables to point left and right of the list 
+excluding pivot
+3. Left points to the low index
+4. Right points to the high
+5. While value at left is less than pivot move right
+6. While value at right is greater than pivot move left
+7. If both step 5 and step 6 are done then swap both left and right element.
+8. If left ≥ right, the point where they met is new pivot
+-----------------------------------------------------------------------------------------
 int split(int a[], int lower, int upper)
 {
 	int pivot = a[lower];
