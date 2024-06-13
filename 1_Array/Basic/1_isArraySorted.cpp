3b305f6229ca42e16b24643e58d326e60e972f00
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-06-13 15:37:54 +0530

    Update 1_isArraySorted.cpp

--- 1_Array/Basic/1_isArraySorted.cpp
+++ 1_Array/Basic/1_isArraySorted.cpp
@@ -4,15 +4,26 @@ Output: True.
 Explanation: The given array is sorted i.e Every element in the array is smaller than or 
 equals to its next values, So the answer is True.
 ----------------------------------------------------------------------------   
-  bool isSorted(int arr[], int n) 
+#include<bits/stdc++.h>
+using namespace std;
+
+bool isSorted(int arr[], int n) 
 {
   for (int i = 0; i < n-1; i++) 
   {
-    if (arr[i] < arr[i + 1])
+    if (arr[i] > arr[i + 1])
       return false;
   }
 
   return true;
+}
+ 
+int main()
+{
+	int arr[] = {1,2,3,45};
+	int n = sizeof(arr)/sizeof(arr[0]);
+	cout<<isSorted(arr, n)<<endl;
+	return 0;
 }
 Time Complexity: O(N)
 Space Complexity: O(1)
