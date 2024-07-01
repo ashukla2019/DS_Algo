fa1a9da0831cd4a2e4b16d541adc3c267ec0e45b
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-07-01 16:55:04 +0530

    Update 5_Diameter_bt.cpp

--- 7_Tree/BinaryTree/Easy/CodePractice/5_Diameter_bt.cpp
+++ 7_Tree/BinaryTree/Easy/CodePractice/5_Diameter_bt.cpp
@@ -1,7 +1,8 @@
 /*Diameter: The diameter of a binary tree is the length of the longest path between any two nodes in a tree. This path may or may not pass through the root.
 The length of a path between two nodes is represented by the number of edges between them.
 Approach:
-1) Call height function to calculate height at every node and update diameter.
+1) Since Daimeter is max path between two nodes => (ltree + rtree) at any node => ltree/rthree of nth node would be height of n-1th node
+   we should calculate and return height of every node, that would become ltree or rtee
 2) Diameter = max(diameter, ltree + rtree)
 */  
 
