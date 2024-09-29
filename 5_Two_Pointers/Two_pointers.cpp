d8cf6665a200cd4f742d2183d033271c75f19d3c
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-09-30 00:10:19 +0530

    Updated Two_pointers.cpp

--- 5_Two_Pointers/Two_pointers.cpp
+++ 5_Two_Pointers/Two_pointers.cpp
@@ -977,39 +977,4 @@ Time Complexity: O(m log m + n log n)
 This algorithm takes O(m log m + n log n) time to sort and O(m + n) time to find the minimum difference. Therefore, the overall runtime is O(m log m + n log n). 
 Auxiliary Space: O(1)
 
---------------------------------------------------------------------------------
-20) Rotate Array:
-Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
-
-Example 1:
-
-Input: nums = [1,2,3,4,5,6,7], k = 3
-Output: [5,6,7,1,2,3,4]
-Explanation:
-rotate 1 steps to the right: [7,1,2,3,4,5,6]
-rotate 2 steps to the right: [6,7,1,2,3,4,5]
-rotate 3 steps to the right: [5,6,7,1,2,3,4]
-Example 2:
-
-Input: nums = [-1,-100,3,99], k = 2
-Output: [3,99,-1,-100]
-Explanation: 
-rotate 1 steps to the right: [99,-1,-100,3]
-rotate 2 steps to the right: [3,99,-1,-100]
-
-void rotate(vector<int>& nums, int k) {
-        int i, j;
-        while(k>0)
-        {
-            j = nums.size() - 1;
-            int temp;
-            temp = nums[j];
-            for(i = j-1; i>=0; i--)
-                nums[i+1] = nums[i];
-            
-            nums[0] = temp;
-            k--;
-        }
-    }
-
 -----------------------------------------------------------------------------------
