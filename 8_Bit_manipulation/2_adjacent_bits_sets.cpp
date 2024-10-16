c60413d71c2a469552548c85806c7a1e686eb7b7
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2024-10-16 20:49:21 +0530

    Update 2_adjacent_bits_sets.cpp

--- 8_Bit_manipulation/2_adjacent_bits_sets.cpp
+++ 8_Bit_manipulation/2_adjacent_bits_sets.cpp
@@ -1,4 +1,11 @@
-//Check if adjacent bits are set in the binary representation of a number:
+Given a number you have to check whether there is pair of adjacent set bit or not.
+
+Input : N = 67
+Output : Yes
+There is a pair of adjacent set bit
+The binary representation is 100011
+
+
 //The expression n & (n << 1) or n & (n >> 1) returns true if n contains any 
 //pair whose adjacent bits are 1
 // Returns true if adjacent bits are set in a binary representation of `n`
