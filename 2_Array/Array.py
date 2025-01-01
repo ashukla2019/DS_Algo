2cc295dc4cdcc31fd1a1a8706e2b037088e34ae1
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-01-01 21:11:59 +0530

    Update Array.py

--- 2_Array/Array.py
+++ 2_Array/Array.py
@@ -1,1 +1,11 @@
+1. Linear search:
+def linearSearch(arr: list[int], target: int):
+    for num in arr:
+        if num == target:
+            return True
 
+    return False
+
+mylist = [12, 10, 32, 1]
+print(linearSearch(mylist, 10))
+---------------------------------------------------------------------
