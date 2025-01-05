c8492f17c018bdc54c3f298e9124190fcc068006
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-01-05 18:55:52 +0530

    Update Array.py

--- 2_Array/Array.py
+++ 2_Array/Array.py
@@ -9,3 +9,29 @@ def linearSearch(arr: list[int], target: int):
 mylist = [12, 10, 32, 1]
 print(linearSearch(mylist, 10))
 ---------------------------------------------------------------------
+2. Second Largest in array:
+def getSecondLargest(self, arr):
+        firstLargest = arr[0]
+        secondLargest = -1
+        
+        for num in arr:
+            
+            if num>firstLargest:
+                secondLargest = firstLargest
+                firstLargest = num
+            elif num<firstLargest and num>secondLargest:
+                secondLargest = num
+        return secondLargest
+-------------------------------------------------------------------------------- 
+3. Check if array is sorted:
+array[] = {1,2,3,4,5} => yes
+
+for i in range(len(arr)):
+    if arr[i+1]>arr[i]
+  
+return true;
+
+Time Complexity: O(N) 
+Space Complexity: O(1)	
+    
+    
