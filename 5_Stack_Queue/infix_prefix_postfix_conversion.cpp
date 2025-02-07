d753b01d101fa3e142f5121ae0e8e79882218f75
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-02-07 12:46:54 +0530

    Update infix_prefix_postfix_conversion.cpp

--- 5_Stack_Queue/infix_prefix_postfix_conversion.cpp
+++ 5_Stack_Queue/infix_prefix_postfix_conversion.cpp
@@ -2,10 +2,25 @@ Operations:
 ^ ->high priority(lets say 3)
 *, / ->medium priority(lets say 2)
 +, -  ->low priority(lets say 1)
+others -> least priority(-1)
 
 Operand: 
 A to Z
 a to z
 0 to 9
+
+Infix: All operators would be in between
+(p+q)*(m-n)
+
+prefix: All operators would be pre
+*+pq-mn
+
+postfix:: All operators would be post
+pq+mn-*
+
+Infix to postfix:
+
+
+  
 
   
