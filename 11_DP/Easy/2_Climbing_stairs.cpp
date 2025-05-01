b3b15e05e2dca354064266b147a6fd8c9b3c7f19
ashukla2019 <54985423+ashukla2019@users.noreply.github.com>
2025-05-01 14:50:43 +0530

    Update 2_Climbing_stairs.cpp

--- 11_DP/Easy/2_Climbing_stairs.cpp
+++ 11_DP/Easy/2_Climbing_stairs.cpp
@@ -29,19 +29,36 @@ int climbStairs(int n)
             return dp[n];
 	    return dp[n]=climbStairs(n-1)+climbStairs(n-2);
     }
+-------------------------------------
+Tabulation:
+#include <iostream>
+#include <vector>
 
-//Tabulation:
-int climbStairs(int n) 
-{
-	vector<int>dp(n+1, -1);
-	//Base case: 
-	//if n=1, there is only one way(1 step move) and n=0 then there is only one way.
-	dp[0]=1;
-	dp[1]=1;
-    
-	for(int i=2; i<=n; i++)
-	{
-		dp[i]=dp(i-1)+dp(i-2);
-	}
-	return dp[n];
+using namespace std;
+
+int climbStairs(int n) {
+    if (n <= 2) {
+        return n;
+    }
+
+    vector<int> dp(n + 1);
+    dp[1] = 1;
+    dp[2] = 2;
+
+    for (int i = 3; i <= n; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2];
+    }
+
+    return dp[n];
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of stairs: ";
+    cin >> n;
+
+    int ways = climbStairs(n);
+    cout << "Number of ways to climb " << n << " stairs: " << ways << endl;
+
+    return 0;
 }
